d2925d2906e30739d3aaf1ea59f20e82192fe0d5
Paul <76972320+MrPaulon@users.noreply.github.com>
2023-04-20 15:56:46 +0200

    Navbar modification
    Fade out & Fade in on slide
    Position now Fixed

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,7 +1,46 @@
 var whatBgIsActive = 1;
+var navIsActive = true;
+var lastposition = 0;
+var canChangeNav = true;
+
+function canChange() {
+    setTimeout(() => {
+        canChangeNav = true;
+    }, "1000");
+}
 
 document.addEventListener("scroll", (event) => {
     scrollpos = window.scrollY;
+
+
+    // Aparition menu top
+
+
+    if (lastposition < scrollpos && navIsActive && canChangeNav) {
+        canChangeNav = false;
+        document.getElementById('nav').style.animation="disparition-top ease 0.5s";
+        setTimeout(() => {
+            document.getElementById('nav').style.display = "none";
+        }, "490");
+        navIsActive = false;
+        canChange()
+    } else {
+        if (lastposition > scrollpos && !navIsActive && canChangeNav) {
+            canChangeNav = false;
+            document.getElementById('nav').style.animation="apparition-top ease 0.5s";
+            setTimeout(() => {
+                document.getElementById('nav').style.display = "inline-flex";
+            }, "10");
+            setTimeout(() => {
+                navIsActive = true;
+            }, "490");
+            canChange()
+        }
+    }
+
+
+
+    lastposition = scrollpos;
 
 
     // Changement de Bg
@@ -17,6 +56,4 @@ document.addEventListener("scroll", (event) => {
             whatBgIsActive = 1;
         }
     }
-
-    
 });
