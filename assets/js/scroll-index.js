71a02a8262ca68222404e6c22fa7e65246be9f76
Paul <76972320+MrPaulon@users.noreply.github.com>
2023-04-23 15:58:46 +0200

    Update files links

--- assets/js/scroll-index.js
+++ assets/js/scroll-index.js
@@ -39,6 +39,25 @@ document.addEventListener("scroll", (event) => {
     }
 
 
+    // Animation Monitor
+
+
+    var monitorMargin = document.getElementById("monitor").offsetTop;
+    var px = scrollpos/2000;
+    
+    if (scrollpos > 3450) {
+        if (lastposition < scrollpos && monitorMargin < 500) {
+            var newTop = monitorMargin + px;
+            document.getElementById("monitor").style.top = ""+newTop+"px";
+        } else {
+            if (monitorMargin > 340) {
+                var newTop = monitorMargin - px;
+                document.getElementById("monitor").style.top = ""+newTop+"px";
+            }
+        }
+    }
+
+
     // Aparition menu top
 
 
@@ -67,4 +86,19 @@ document.addEventListener("scroll", (event) => {
 
 
     lastposition = scrollpos;
+
+
+    // Changement de Bg
+     
+    if (scrollpos > 1250 && whatBgIsActive!= 2) {
+        document.getElementById("backgroundimg").src = "assets/img/rainbow62.gif";
+        document.getElementById("backgroundimg").style.filter = "grayscale(0.4) blur(2px)"
+        whatBgIsActive = 2;
+    } else {
+        if (scrollpos < 1250 && whatBgIsActive!=1) {
+            document.getElementById("backgroundimg").src = "assets/img/bg3.gif";
+            document.getElementById("backgroundimg").style.filter = "grayscale(0.9) blur(2px)"
+            whatBgIsActive = 1;
+        }
+    }
 });
